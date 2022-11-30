0e868ff6e9a8bc08cd0c0461b6632c5adcd011c1
Felipe Eduardo <Felipeeduardol7@outlook.com>
2022-11-30 17:06:56 -0300

    HELLO WORLD

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,95 @@
+import { HandPalm, Play } from 'phosphor-react'
+import { useForm, FormProvider } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
+import { useContext } from 'react'
+
+import {
+  HomeContainer,
+  StartCountdownButton,
+  StopCountdownButton,
+} from './styles'
+import { NewCycleForm } from './components/NewCycleForm'
+import { Countdown } from './components/Countdown'
+import { CyclesContext } from '../../contexts/CyclesContext'
+
+/* 
+  exemplo do register
+
+  ° function register(name:string) {
+    return {
+      onChange: () => void,
+      onBlur: () => void,
+      onFocus: () => void,
+      ...
+    }
+  }
+*/
+
+/* interface NewCycleFormData {
+  task: string
+  minutesAmount: number
+} */
+
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minutesAmount: zod
+    .number()
+    .min(5, 'o ciclo precisa ser de no mínimo 5 minutos')
+    .max(60, 'o ciclo precisa ser de no máximo 60 minutos'),
+})
+
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
+export function Home() {
+  const { activeCycle, createNewCycle, interruptCurrentCycle } =
+    useContext(CyclesContext)
+
+  const newCycleForm = useForm<NewCycleFormData>({
+    // objeto de configuração
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
+  }) // useForm retorna um objeto, ai podemos usar desestruturação. Olhar a função register acima
+
+  const { handleSubmit, watch, reset } = newCycleForm
+
+  function handleCreateNewCycle(data: NewCycleFormData) {
+    createNewCycle(data)
+    reset()
+  }
+
+  const task = watch('task')
+  const isSubmitDisabled = !task
+
+  /* 
+    ° Prop Drilling -> Quando temos MUITAS propriedades APENAS para comunicação entre componentes
+
+    ° Contex API -> Permite compartilharmos informações entre vários componentes ao mesmo tempo
+  */
+
+  return (
+    <HomeContainer>
+      <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
+        <FormProvider {...newCycleForm}>
+          <NewCycleForm />
+        </FormProvider>
+        <Countdown />
+
+        {activeCycle ? (
+          <StopCountdownButton type="button" onClick={interruptCurrentCycle}>
+            <HandPalm size={24} />
+            Interromper
+          </StopCountdownButton>
+        ) : (
+          <StartCountdownButton disabled={isSubmitDisabled} type="submit">
+            <Play size={24} />
+            Começar
+          </StartCountdownButton>
+        )}
+      </form>
+    </HomeContainer>
+  )
+}
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,58 @@
+import styled from 'styled-components'
+
+export const HomeContainer = styled.main`
+  flex: 1;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 5.6rem;
+  }
+`
+
+const BaseCountdownButton = styled.button`
+  width: 100%;
+  border: none;
+  padding: 1.6rem;
+  border-radius: 8px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.8rem;
+
+  font-weight: bold;
+
+  color: ${(props) => props.theme['gray-100']};
+
+  cursor: pointer;
+
+  transition: all 0.2s;
+
+  &:disabled {
+    opacity: 0.7;
+    cursor: not-allowed;
+  }
+`
+
+export const StartCountdownButton = styled(BaseCountdownButton)`
+  background: ${(props) => props.theme['green-500']};
+
+  &:not(:disabled):hover {
+    background: ${(props) => props.theme['green-700']};
+  }
+`
+
+export const StopCountdownButton = styled(BaseCountdownButton)`
+  background: ${(props) => props.theme['red-500']};
+
+  &:not(:disabled):hover {
+    background: ${(props) => props.theme['red-700']};
+  }
+`
